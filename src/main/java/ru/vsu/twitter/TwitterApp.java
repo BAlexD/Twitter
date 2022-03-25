20eccf4e72261d344fdaf294164ebe740ef1c986
mikhail-larionov <60404888+mikhail-larionov@users.noreply.github.com>
2022-03-25 23:06:55 +0300

    TWIT-10: repository was initialized

---
+++ src/main/java/ru/vsu/twitter/TwitterApp.java
@@ -0,0 +1,11 @@
+package ru.vsu.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApp.class, args);
+    }
+}
