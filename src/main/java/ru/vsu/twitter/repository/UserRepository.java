79a32f3863388e0fd00216021c16cc7e311501dd
mikhail-larionov <60404888+mikhail-larionov@users.noreply.github.com>
2022-03-25 23:13:45 +0300

    TWIT-13: user repository was created

---
+++ src/main/java/ru/vsu/twitter/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.vsu.twitter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.vsu.twitter.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
