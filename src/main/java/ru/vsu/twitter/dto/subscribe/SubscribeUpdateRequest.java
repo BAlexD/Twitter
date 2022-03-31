14c44a3ccaa9f1e0e16f22849eadbe2876e46a20
AnnFufaeva <ann.fufaeva@gmail.com>
2022-03-31 21:26:19 +0300

    TWIT-12:  subscribe dto`s were added

---
+++ src/main/java/ru/vsu/twitter/dto/subscribe/SubscribeUpdateRequest.java
@@ -0,0 +1,16 @@
+package ru.vsu.twitter.dto.subscribe;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class SubscribeUpdateRequest {
+    private Long id;
+    private Long profileId; //TODO добавить связь
+    private String subscriberId; //TODO добавить связь
+}
