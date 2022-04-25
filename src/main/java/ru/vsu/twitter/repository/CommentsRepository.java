9ccdd0da3d950d7696d03b514c375ef8fe54695f
mikhail-larionov <60404888+mikhail-larionov@users.noreply.github.com>
2022-04-25 10:29:19 +0300

    TWIT-23: repository refactored

--- src/main/java/ru/vsu/twitter/repository/CommentsRepository.java
+++ src/main/java/ru/vsu/twitter/repository/CommentsRepository.java
@@ -1,14 +1,11 @@
 package ru.vsu.twitter.repository;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.vsu.twitter.entity.Comments;
 
-import java.util.List;
 
 @Repository
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
-//    @Override
-//    public List<Comments> findAll(Sort sort);
+
 }
